45e55cd7af75fc34fa00c925345a84151d01f786
benBen <benmbenjamin@gmail.com>
2020-10-13 13:35:03 -0700

    react port boilerplate

---
+++ react-port/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Scene from './Scene';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Scene />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
