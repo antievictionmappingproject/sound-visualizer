69484eba7b7b07343d2d1b0fe634de5bf402e781
Christopher Tan <christopher.tan17@gmail.com>
2020-10-20 18:48:41 -0700

    Scene.js working

--- react-port/src/index.js
+++ react-port/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Scene from './Scene';
-import Scratch from './Scratch';
+import Scene from './Scene';
+//import Scratch from './Scratch';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Scratch />
+    <Scene />
   </React.StrictMode>,
   document.getElementById('root')
 );
