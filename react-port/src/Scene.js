86e931b0608755ca25912d6e8f3b2914fb7c8a28
Ryan Johnston <ryanj9@stanford.edu>
2020-11-13 18:59:33 -0800

    fixing the colors to the new design

--- react-port/src/Scene.js
+++ react-port/src/Scene.js
@@ -28,6 +28,7 @@ export default (props) => {
     let colorIndex = 0;
     let width = 200;
     let height = 200;
+    let red = "#de3826";
     let circle;
     let diam = 0;
     let circle_arr = [];
@@ -59,12 +60,12 @@ export default (props) => {
         ctx.createCanvas(width, height).parent(canvasParentRef);
         audioRef.current.loop();
         audioRef.current.pause();
-  
+
         timelineActions.forEach((ta, i) => {
           audioRef.current.addCue(ta.seconds, ta.action)
         })
-  
-        ctx.background(255, 0, 0);
+
+        ctx.background(255, 255, 255);
         ampRef.current = new window.p5.Amplitude();
         fftRef.current = new window.p5.FFT();
         setLoaded(true)
@@ -75,41 +76,33 @@ export default (props) => {
       if (!loaded) { return }
       const amp = ampRef.current;
       const fft = fftRef.current;
-      ctx.background(colors[colorIndex]);
+      ctx.background(255, 255, 255);
       var vol = amp.getLevel();
       vol_arr.push(vol);
       if (vol_arr.length > 20) {
         vol_arr.shift();
       }
       diam = ctx.map(vol, 0, 0.1, 20, 400);
+      ctx.noFill();
+      ctx.beginShape();
+      ctx.stroke(red);
+      ctx.strokeWeight(5);
       circle = ctx.ellipse(width / 2, height / 2, diam, diam);
+      ctx.endShape();
 
       vol_avg = average(vol_arr);
-
       if (vol > color_change_rate * vol_avg) {
         change = change + 1;
         if (change == 10) {
           circle_arr.push(new BeatCircle(width / 2, height / 2, ctx));
-          if (audioRef.current.isPlaying()) {
-            change = 0;
-            let spectrum = fft.analyze();
-            let max = spectrum[0];
-            let min = spectrum[0];
-            for (let i = 1; i< spectrum.length; i += 10){
-                if (spectrum[i] > max) max = spectrum[i];
-                if (spectrum[i] < min) min = spectrum[i];
-            }
-            let avg = (max + min) / 2;
-            colorIndex = Math.floor(ctx.map (avg, 0, 255, 0, 280));
-            ctx.background(colors[colorIndex]);
-          }
-        }
+          change = 0;
       }
 
       let waveform = fft.waveform();
       ctx.noFill();
       ctx.beginShape();
-      ctx.stroke(20);
+      ctx.stroke(0, 0, 0);
+      ctx.strokeWeight(2);
       for (let i = 0; i < waveform.length; i++){
         let x = ctx.map(i, 0, waveform.length, 0, width);
         let y = ctx.map( waveform[i], -1, 1, 0, height);
@@ -147,8 +140,8 @@ class BeatCircle {
     this.lifespan -= 2.0;
   }
   display() {
-    this.ctx.stroke(255, this.lifespan);
-    this.ctx.strokeWeight(2);
+    this.ctx.stroke(red);
+    this.ctx.strokeWeight(5);
     this.ctx.noFill();
     this.ctx.ellipse(this.x, this.y, this.r, this.r);
   }
